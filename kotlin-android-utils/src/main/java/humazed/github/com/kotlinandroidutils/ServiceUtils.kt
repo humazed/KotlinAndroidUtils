54536cf0fa65e72b1132ab9bbb17259048008c75
humazed <humazed@gmail.com>
2018-02-28 01:48:20 +0200

    added ServiceUtils.kt

---
+++ kotlin-android-utils/src/main/java/humazed/github/com/kotlinandroidutils/ServiceUtils.kt
@@ -0,0 +1,23 @@
+package humazed.github.com.kotlinandroidutils
+
+import android.app.ActivityManager
+import android.content.Context
+import android.util.Log
+
+/**
+ * Check if the service is running in the device.
+ *
+ * @param serviceClass the service class
+ * @return whether the service is running
+ */
+fun Context.isServiceRunning(serviceClass: Class<*>): Boolean {
+    try {
+        val manager = getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
+        for (service in manager.getRunningServices(Integer.MAX_VALUE)) {
+            if (serviceClass.name == service.service.className) return true
+        }
+    } catch (e: Exception) {
+        Log.e("ServiceUtils", Log.getStackTraceString(e))
+    }
+    return false
+}
