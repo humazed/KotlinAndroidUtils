8925bb41f199b2ba917fd577f2217794641f0787
ibrahim <humazed@gmail.com>
2018-07-30 00:32:14 +0200

    added start for startActivity

--- kotlin-android-utils/src/main/java/humazed/github/com/kotlinandroidutils/ActivityUtils.kt
+++ kotlin-android-utils/src/main/java/humazed/github/com/kotlinandroidutils/ActivityUtils.kt
@@ -1,6 +1,8 @@
 package humazed.github.com.kotlinandroidutils
 
 import android.app.Activity
+import android.content.Context
+import android.content.Intent
 import android.os.Build
 import android.view.View
 
@@ -22,4 +24,28 @@ fun Activity.setImmersiveScreen() {
                 or View.SYSTEM_UI_FLAG_FULLSCREEN
                 or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
     }
+}
+
+/**
+ * Example:
+ * ```
+ * start<MainActivity> {
+ *    flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_CLEAR_TASK
+ * }
+ * ```
+ */
+inline fun <reified A : Activity> Context.start(configIntent: Intent.() -> Unit = {}) {
+    startActivity(Intent(this, A::class.java).apply(configIntent))
+}
+
+/**
+ * Example:
+ * ```
+ * startActivity(Settings.ACTION_APPLICATION_DETAILS_SETTINGS) {
+ *     data = Uri.fromParts("package", packageName, null)
+ * }
+ * ```
+ */
+inline fun Context.startActivity(action: String, configIntent: Intent.() -> Unit = {}) {
+    startActivity(Intent(action).apply(configIntent))
 }
