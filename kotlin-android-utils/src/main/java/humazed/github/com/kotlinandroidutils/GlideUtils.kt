5945408072f21250428827173fdf26346db02117
humazed <humazed@gmail.com>
2019-01-27 13:31:31 +0200

    added GlideUtils

---
+++ kotlin-android-utils/src/main/java/humazed/github/com/kotlinandroidutils/GlideUtils.kt
@@ -0,0 +1,9 @@
+package humazed.github.com.kotlinandroidutils
+
+import android.content.Context
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+
+fun ImageView.load(context: Context, url: String) {
+    Glide.with(context).load(url).into(this)
+}
