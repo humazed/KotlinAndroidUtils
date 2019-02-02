f9eef786cf0d73c0fd3f79a595d6f6c0354e9641
humazed <humazed@gmail.com>
2019-02-02 17:09:17 +0200

    added SpinnerUtils.kt

---
+++ kotlin-android-utils/src/main/java/humazed/github/com/kotlinandroidutils/SpinnerUtils.kt
@@ -0,0 +1,23 @@
+package humazed.github.com.kotlinandroidutils
+
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+
+
+fun <T> Spinner.onItemSelected(items: List<T>, names: List<String>, onItemSelected: (item: T, position: Int) -> Unit) {
+    adapter = ArrayAdapter<String>(context, android.R.layout.simple_spinner_dropdown_item, names)
+    onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+        override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+            onItemSelected(items[position], position)
+        }
+
+        override fun onNothingSelected(parent: AdapterView<*>?) {
+        }
+    }
+}
+
+fun <T> Spinner.onItemSelected(items: List<T>, names: List<String>, onItemSelected: (item: T) -> Unit) {
+    onItemSelected(items, names) { item, _ -> onItemSelected(item) }
+}
