a2c62536a7deb6e211f393d9655d1b49fc215340
humazed <humazed@gmail.com>
2019-02-10 19:54:21 +0200

    remove context parameter from GlideUtils.kt methods added method for loading image from Uri

--- kotlin-android-utils/src/main/java/humazed/github/com/kotlinandroidutils/GlideUtils.kt
+++ kotlin-android-utils/src/main/java/humazed/github/com/kotlinandroidutils/GlideUtils.kt
@@ -1,9 +1,14 @@
 package humazed.github.com.kotlinandroidutils
 
-import android.content.Context
+import android.net.Uri
 import android.widget.ImageView
 import com.bumptech.glide.Glide
 
-fun ImageView.load(context: Context, url: String?) {
+fun ImageView.load(url: String?) {
     if (!url.isNullOrBlank()) Glide.with(context).load(url).into(this)
 }
+
+fun ImageView.load(url: Uri?) {
+    if (url != null) Glide.with(context).load(url).into(this)
+}
+
