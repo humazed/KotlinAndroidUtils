f2de2f85b0421caff00efc8bbc4ea67e717bb6bc
humazed <humazed@gmail.com>
2019-02-10 17:52:08 +0200

    fix imagepicker so it gets image from camera correctly

--- kotlin-android-utils/src/main/java/humazed/github/com/kotlinandroidutils/ImagePicker.kt
+++ kotlin-android-utils/src/main/java/humazed/github/com/kotlinandroidutils/ImagePicker.kt
@@ -4,16 +4,22 @@ import android.Manifest
 import android.app.Activity
 import android.app.Dialog
 import android.content.ContentUris
+import android.content.Context
 import android.content.Intent
+import android.net.Uri
+import android.os.Environment
 import android.provider.MediaStore
 import android.view.Gravity
 import android.widget.LinearLayout
+import androidx.core.content.FileProvider
 import com.gun0912.tedpermission.PermissionListener
 import com.gun0912.tedpermission.TedPermission
 import org.jetbrains.anko.longToast
 import stream.customimagepicker.CustomImagePicker
 import stream.jess.ui.TwoWayGridView
 import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
 import java.util.*
 
 private const val CAPTURE_IMAGE = 0
@@ -36,10 +42,7 @@ fun Activity.pickImage(onItemSelected: (imageFile: File) -> Unit) {
     val layoutCamera = bottomSheet.findViewById<LinearLayout>(R.id.btn_camera)
     val layoutGallery = bottomSheet.findViewById<LinearLayout>(R.id.btn_gallery)
     layoutCamera.setOnClickListener {
-        val cameraIntent = Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE)
-
-//        startActivityForResult(cameraIntent, CAPTURE_IMAGE)
-        startActivityForResult(cameraIntent) { data ->
+        dispatchTakePicture { data ->
             data.data?.let { imageUri ->
                 getFilePath(imageUri)?.let {
                     onItemSelected(File(it))
@@ -51,8 +54,6 @@ fun Activity.pickImage(onItemSelected: (imageFile: File) -> Unit) {
     layoutGallery.setOnClickListener {
         val photoPickerIntent = Intent(Intent.ACTION_PICK)
         photoPickerIntent.type = "image/*"
-
-//        startActivityForResult(photoPickerIntent, SELECT_PHOTO)
         startActivityForResult(photoPickerIntent) { data ->
             data.data?.let { imageUri ->
                 getFilePath(imageUri)?.let {
@@ -83,16 +84,6 @@ fun Activity.pickImage(onItemSelected: (imageFile: File) -> Unit) {
     bottomSheetDialog.show()
 }
 
-fun Activity.imagePickerOnActivityResult(requestCode: Int, resultCode: Int, data: Intent?): File? {
-    val imageUri = when (requestCode) {
-        CAPTURE_IMAGE -> if (resultCode == Activity.RESULT_OK) data?.data else null
-        SELECT_PHOTO -> if (resultCode == Activity.RESULT_OK) data?.data else null
-        else -> null
-    }
-
-    return imageUri?.let { File(getFilePath(it)) }
-}
-
 fun Activity.pickImageWithPermission(onItemSelected: (imageFile: File) -> Unit): Unit {
     val permissionListener = object : PermissionListener {
         override fun onPermissionGranted() {
@@ -108,4 +99,48 @@ fun Activity.pickImageWithPermission(onItemSelected: (imageFile: File) -> Unit):
             .setDeniedMessage("If you reject permission,you can not use this service\n\nPlease turn on permissions at [Setting] > [Permission]")
             .setPermissions(Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.CAMERA)
             .check()
+}
+
+private fun Context.dispatchTakePicture(onSuccess: (data: Intent) -> Unit) {
+    Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+        // Ensure that there's a camera activity to handle the intent
+        takePictureIntent.resolveActivity(packageManager)?.also {
+            // Create the File where the photo should go
+            val photoFile: File? = try {
+                createImageFile()
+            } catch (ex: IOException) {
+                // Error occurred while creating the File
+                null
+            }
+            // Continue only if the File was successfully created
+            photoFile?.also {
+                val photoURI: Uri = FileProvider.getUriForFile(this, "com.example.android.fileprovider", it)
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                startActivityForResult(takePictureIntent) { data ->
+                    galleryAddPic(photoFile)
+                    onSuccess(data)
+                }
+            }
+        }
+    }
+}
+
+
+@Throws(IOException::class)
+private fun Context.createImageFile(): File {
+    // Create an image file name
+    val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US).format(Date())
+    val storageDir: File = getExternalFilesDir(Environment.DIRECTORY_PICTURES)!!
+    return File.createTempFile(
+            "JPEG_${timeStamp}_", /* prefix */
+            ".jpg", /* suffix */
+            storageDir /* directory */
+    )
+}
+
+private fun Context.galleryAddPic(imageFile: File) {
+    Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE).also { mediaScanIntent ->
+        mediaScanIntent.data = Uri.fromFile(imageFile)
+        sendBroadcast(mediaScanIntent)
+    }
 }
