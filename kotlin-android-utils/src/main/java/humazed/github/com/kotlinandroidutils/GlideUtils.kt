1da78b8c47a82af4a5b53d7c15f5f6b0b798f4c8
humazed <humazed@gmail.com>
2019-02-23 21:03:49 +0200

    use glide 4.8.0 as 4.9.0 causes crash with CustomImagePicker

--- kotlin-android-utils/src/main/java/humazed/github/com/kotlinandroidutils/GlideUtils.kt
+++ kotlin-android-utils/src/main/java/humazed/github/com/kotlinandroidutils/GlideUtils.kt
@@ -4,25 +4,16 @@ import android.content.Context
 import android.net.Uri
 import android.widget.ImageView
 import com.bumptech.glide.Glide
-import com.bumptech.glide.Registry
-import com.bumptech.glide.annotation.GlideModule
-import com.bumptech.glide.integration.okhttp3.OkHttpUrlLoader
-import com.bumptech.glide.load.engine.DiskCacheStrategy
-import com.bumptech.glide.load.model.GlideUrl
-import com.bumptech.glide.module.AppGlideModule
 import com.bumptech.glide.request.RequestOptions
-import okhttp3.OkHttpClient
-import java.io.InputStream
-import java.util.concurrent.TimeUnit
 
 fun ImageView.load(url: String?) {
-    if (!url.isNullOrBlank()) Glide.with(context).load(url).into(this)
+    if (!url.isNullOrBlank()) customGlide(context).load(url).into(this)
 }
 
 fun ImageView.load(url: Uri?) {
-    if (url != null) Glide.with(context)
-            .applyDefaultRequestOptions(RequestOptions.timeoutOf(5 * 60 * 1000).diskCacheStrategy(DiskCacheStrategy.ALL))
-            .load(url)
-            .apply(RequestOptions.timeoutOf(5 * 60 * 1000))
-            .into(this)
+    if (url != null) customGlide(context).load(url).into(this)
 }
+
+fun customGlide(context: Context) =
+        Glide.with(context)
+                .applyDefaultRequestOptions(RequestOptions.timeoutOf(5 * 60 * 1000))
