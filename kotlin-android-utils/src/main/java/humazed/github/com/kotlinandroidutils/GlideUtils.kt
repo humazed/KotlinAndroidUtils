87afbe313fa7b7e78998d4b8f6c73b0e6550f4d9
humazed <humazed@gmail.com>
2019-02-23 18:49:38 +0200

    remove AppGlideModule to avoid crashing in app

--- kotlin-android-utils/src/main/java/humazed/github/com/kotlinandroidutils/GlideUtils.kt
+++ kotlin-android-utils/src/main/java/humazed/github/com/kotlinandroidutils/GlideUtils.kt
@@ -7,31 +7,22 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.Registry
 import com.bumptech.glide.annotation.GlideModule
 import com.bumptech.glide.integration.okhttp3.OkHttpUrlLoader
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.load.model.GlideUrl
-import com.bumptech.glide.module.LibraryGlideModule
+import com.bumptech.glide.module.AppGlideModule
+import com.bumptech.glide.request.RequestOptions
 import okhttp3.OkHttpClient
 import java.io.InputStream
 import java.util.concurrent.TimeUnit
 
 fun ImageView.load(url: String?) {
-    if (!url.isNullOrBlank()) GlideApp.with(context).load(url).into(this)
+    if (!url.isNullOrBlank()) Glide.with(context).load(url).into(this)
 }
 
 fun ImageView.load(url: Uri?) {
-    if (url != null) GlideApp.with(context).load(url).into(this)
-}
-
-
-@GlideModule
-class CustomGlideModule : LibraryGlideModule() {
-    override fun registerComponents(context: Context, glide: Glide, registry: Registry) {
-        val client = OkHttpClient.Builder()
-                .readTimeout(5, TimeUnit.MINUTES)
-                .writeTimeout(5, TimeUnit.MINUTES)
-                .callTimeout(5, TimeUnit.MINUTES)
-                .connectTimeout(5, TimeUnit.MINUTES)
-                .build()
-
-        glide.registry.replace(GlideUrl::class.java, InputStream::class.java, OkHttpUrlLoader.Factory(client))
-    }
+    if (url != null) Glide.with(context)
+            .applyDefaultRequestOptions(RequestOptions.timeoutOf(5 * 60 * 1000).diskCacheStrategy(DiskCacheStrategy.ALL))
+            .load(url)
+            .apply(RequestOptions.timeoutOf(5 * 60 * 1000))
+            .into(this)
 }
