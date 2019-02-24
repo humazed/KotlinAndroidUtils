612064c363489b914982fb2f2e579205836d68f1
humazed <humazed@gmail.com>
2019-02-24 17:52:53 +0200

    added load from file to GlideUtils.kt

--- kotlin-android-utils/src/main/java/humazed/github/com/kotlinandroidutils/GlideUtils.kt
+++ kotlin-android-utils/src/main/java/humazed/github/com/kotlinandroidutils/GlideUtils.kt
@@ -5,6 +5,7 @@ import android.net.Uri
 import android.widget.ImageView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
+import java.io.File
 
 fun ImageView.load(url: String?) {
     if (!url.isNullOrBlank()) customGlide(context).load(url).into(this)
@@ -12,6 +13,10 @@ fun ImageView.load(url: String?) {
 
 fun ImageView.load(url: Uri?) {
     if (url != null) customGlide(context).load(url).into(this)
+}
+
+fun ImageView.load(file: File?) {
+    if (file != null) customGlide(context).load(file).into(this)
 }
 
 fun customGlide(context: Context) =
