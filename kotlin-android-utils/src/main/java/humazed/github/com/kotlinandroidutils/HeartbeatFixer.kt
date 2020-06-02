8bd6389088527e9b1d9df61c22aa23f0fcad559d
ibrahim <humazed@gmail.com>
2020-06-02 05:09:00 +0200

    added triggerHeartbeat extension on fragment

--- kotlin-android-utils/src/main/java/humazed/github/com/kotlinandroidutils/HeartbeatFixer.kt
+++ kotlin-android-utils/src/main/java/humazed/github/com/kotlinandroidutils/HeartbeatFixer.kt
@@ -2,9 +2,14 @@ package humazed.github.com.kotlinandroidutils
 
 import android.content.Context
 import android.content.Intent
+import androidx.fragment.app.Fragment
 
 
 fun Context.triggerHeartbeat() {
     sendBroadcast(Intent("com.google.android.intent.action.GTALK_HEARTBEAT"))
     sendBroadcast(Intent("com.google.android.intent.action.MCS_HEARTBEAT"))
+}
+
+fun Fragment.triggerHeartbeat() {
+    context?.triggerHeartbeat()
 }
