165f21bb07a62e8db5f179d8fd0731382b581aa5
ibrahim <humazed@gmail.com>
2020-07-22 12:52:59 +0200

    1.11.1 updated imageview to use glide in case of url

--- kotlin-android-utils/src/main/java/humazed/github/com/kotlinandroidutils/ImageFullScreenActivity.kt
+++ kotlin-android-utils/src/main/java/humazed/github/com/kotlinandroidutils/ImageFullScreenActivity.kt
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.activity_image_full_screen.*
 
 const val KEY_FULL_SCREEN_IMAGE_URI = "ImageFullScreenActivity:KEY_IMAGE_URI"
+const val KEY_FULL_SCREEN_IMAGE_URL = "ImageFullScreenActivity:KEY_IMAGE_URL"
 
 class ImageFullScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,7 +16,7 @@ class ImageFullScreenActivity : AppCompatActivity() {
         setContentView(R.layout.activity_image_full_screen)
 
         photoView.setImageURI(intent.getParcelableExtra(KEY_FULL_SCREEN_IMAGE_URI))
-
+        photoView.load(intent.getStringExtra(KEY_FULL_SCREEN_IMAGE_URL))
     }
 
 }
@@ -25,14 +26,12 @@ fun Context.openImageFullScreen(imageUri: Uri) {
     start<ImageFullScreenActivity>(KEY_FULL_SCREEN_IMAGE_URI to imageUri)
 }
 
-fun Fragment.openImageFullScreen(imageUri: Uri) {
-    start<ImageFullScreenActivity>(KEY_FULL_SCREEN_IMAGE_URI to imageUri)
-}
+fun Fragment.openImageFullScreen(imageUri: Uri) = context?.openImageFullScreen(imageUri)
+
 
 fun Context.openImageFullScreen(imageUrl: String) {
-    start<ImageFullScreenActivity>(KEY_FULL_SCREEN_IMAGE_URI to Uri.parse(imageUrl))
+    start<ImageFullScreenActivity>(KEY_FULL_SCREEN_IMAGE_URL to imageUrl)
 }
 
-fun Fragment.openImageFullScreen(imageUrl: String) {
-    start<ImageFullScreenActivity>(KEY_FULL_SCREEN_IMAGE_URI to Uri.parse(imageUrl))
-}
+fun Fragment.openImageFullScreen(imageUrl: String) = context?.openImageFullScreen(imageUrl)
+
