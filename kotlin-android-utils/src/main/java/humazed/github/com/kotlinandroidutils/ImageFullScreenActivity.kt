1ba198cae9405becfd55c78fe4c62971f96b96e6
ibrahim <humazed@gmail.com>
2020-07-22 12:40:03 +0200

    1.11.0 updated glide added openImageFullScreen updated searchy2:CustomImagePicker to humazed:CustomImagePicker

--- kotlin-android-utils/src/main/java/humazed/github/com/kotlinandroidutils/ImageFullScreenActivity.kt
+++ kotlin-android-utils/src/main/java/humazed/github/com/kotlinandroidutils/ImageFullScreenActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.activity_image_full_screen.*
 
 const val KEY_FULL_SCREEN_IMAGE_URI = "ImageFullScreenActivity:KEY_IMAGE_URI"
@@ -17,13 +18,21 @@ class ImageFullScreenActivity : AppCompatActivity() {
 
     }
 
-    companion object {
-        fun Context.start(imageUri: Uri) {
-            start<ImageFullScreenActivity>(KEY_FULL_SCREEN_IMAGE_URI to imageUri)
-        }
+}
 
-        fun Context.start(imageUrl: String) {
-            start<ImageFullScreenActivity>(KEY_FULL_SCREEN_IMAGE_URI to Uri.parse(imageUrl))
-        }
-    }
+
+fun Context.openImageFullScreen(imageUri: Uri) {
+    start<ImageFullScreenActivity>(KEY_FULL_SCREEN_IMAGE_URI to imageUri)
+}
+
+fun Fragment.openImageFullScreen(imageUri: Uri) {
+    start<ImageFullScreenActivity>(KEY_FULL_SCREEN_IMAGE_URI to imageUri)
+}
+
+fun Context.openImageFullScreen(imageUrl: String) {
+    start<ImageFullScreenActivity>(KEY_FULL_SCREEN_IMAGE_URI to Uri.parse(imageUrl))
+}
+
+fun Fragment.openImageFullScreen(imageUrl: String) {
+    start<ImageFullScreenActivity>(KEY_FULL_SCREEN_IMAGE_URI to Uri.parse(imageUrl))
 }
