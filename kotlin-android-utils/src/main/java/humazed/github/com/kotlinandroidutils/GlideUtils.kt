464a778520035aabe951b9b389d3f4230cd34e6f
ibrahim <humazed@gmail.com>
2020-09-17 13:38:25 +0200

    1.13.0 added options to GlideUtils.kt

--- kotlin-android-utils/src/main/java/humazed/github/com/kotlinandroidutils/GlideUtils.kt
+++ kotlin-android-utils/src/main/java/humazed/github/com/kotlinandroidutils/GlideUtils.kt
@@ -7,18 +7,18 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import java.io.File
 
-fun ImageView.load(url: String?) {
-    if (!url.isNullOrBlank()) customGlide(context).load(url).into(this)
+fun ImageView.load(url: String?, options: RequestOptions.() -> Unit = {}) = customGlide(context, options).also {
+    if (!url.isNullOrBlank()) it.load(url).into(this)
 }
 
-fun ImageView.load(url: Uri?) {
-    if (url != null) customGlide(context).load(url).into(this)
+fun ImageView.load(url: Uri?, options: RequestOptions.() -> Unit = {}) = customGlide(context, options).also {
+    if (url != null) it.load(url).into(this)
 }
 
-fun ImageView.load(file: File?) {
-    if (file != null) customGlide(context).load(file).into(this)
+fun ImageView.load(file: File?, options: RequestOptions.() -> Unit = {}) = customGlide(context, options).also {
+    if (file != null) it.load(file).into(this)
 }
 
-fun customGlide(context: Context) =
+fun customGlide(context: Context, options: RequestOptions.() -> Unit) =
         Glide.with(context)
-                .applyDefaultRequestOptions(RequestOptions.timeoutOf(5 * 60 * 1000))
+                .applyDefaultRequestOptions(RequestOptions.timeoutOf(5 * 60 * 1000).apply(options))
--- kotlin-android-utils/src/main/java/humazed/github/com/kotlinandroidutils/TabLayoutUtils.kt
+++ kotlin-android-utils/src/main/java/humazed/github/com/kotlinandroidutils/TabLayoutUtils.kt
@@ -5,11 +5,20 @@ import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
+import androidx.lifecycle.Lifecycle
 import androidx.viewpager.widget.ViewPager
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
+
+data class Tab(val fragment: Fragment, @StringRes val titleRes: Int? = null, @DrawableRes val iconRes: Int? = null)
+
+// region the legacy ViewPager
 fun AppCompatActivity.setupTabs(tabLayout: TabLayout?, viewPager: ViewPager?, tabs: List<Tab>) {
     viewPager?.adapter = SectionsPagerAdapter(this, supportFragmentManager, tabs)
     tabLayout?.setupWithViewPager(viewPager)
@@ -30,10 +39,51 @@ fun Fragment.setupTabs(tabLayout: TabLayout?, viewPager: ViewPager?, tabs: List<
     }
 }
 
-data class Tab(val fragment: Fragment, @StringRes val titleRes: Int? = null, @DrawableRes val iconRes: Int? = null)
 
-class SectionsPagerAdapter(val context: Context, fm: FragmentManager, private val tabs: List<Tab>) : FragmentPagerAdapter(fm) {
+class SectionsPagerAdapter(val context: Context, fm: FragmentManager, private val tabs: List<Tab>) :
+        FragmentPagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
     override fun getItem(position: Int): Fragment = tabs[position].fragment
     override fun getPageTitle(position: Int): CharSequence? = tabs[position].titleRes?.let { context.getString(it) }
     override fun getCount() = tabs.size
 }
+// endregion the legacy ViewPager
+
+// region the ViewPager2
+fun AppCompatActivity.setupTabs(tabLayout: TabLayout?, viewPager: ViewPager2?, tabs: List<Tab>) {
+    viewPager?.adapter = SectionsPager2Adapter(this, tabs)
+
+    if (tabLayout != null && viewPager != null) {
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            tabs[position].apply {
+                tab.text = titleRes?.let { getString(it) } ?: ""
+                iconRes?.let { tab.setIcon(it) }
+            }
+        }.attach()
+    }
+}
+
+fun Fragment.setupTabs(tabLayout: TabLayout?, viewPager: ViewPager2?, tabs: List<Tab>) {
+    viewPager?.adapter = SectionsPager2Adapter(this, tabs)
+
+    if (tabLayout != null && viewPager != null) {
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            tabs[position].apply {
+                tab.text = titleRes?.let { getString(it) } ?: ""
+                iconRes?.let { tab.setIcon(it) }
+            }
+        }.attach()
+    }
+}
+
+private class SectionsPager2Adapter(fm: FragmentManager, lifecycle: Lifecycle, private val tabs: List<Tab>)
+    : FragmentStateAdapter(fm, lifecycle) {
+    constructor(fragment: Fragment, tabs: List<Tab>) : this(fragment.childFragmentManager, fragment.lifecycle, tabs)
+    constructor(activity: FragmentActivity, tabs: List<Tab>) : this(activity.supportFragmentManager, activity.lifecycle, tabs)
+
+    override fun createFragment(position: Int): Fragment = tabs[position].fragment
+
+    override fun getItemCount() = tabs.size
+}
+// endregion the ViewPager2
+
+
