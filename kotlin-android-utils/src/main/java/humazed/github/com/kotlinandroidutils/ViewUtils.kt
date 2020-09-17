b94043614dee2411dcb214a064cffba24e108ba8
ibrahim <humazed@gmail.com>
2020-09-17 19:57:26 +0200

    1.14.1 Handle nulls in ViewUtils.kt

--- kotlin-android-utils/src/main/java/humazed/github/com/kotlinandroidutils/ViewUtils.kt
+++ kotlin-android-utils/src/main/java/humazed/github/com/kotlinandroidutils/ViewUtils.kt
@@ -12,59 +12,63 @@ import androidx.annotation.ColorRes
 import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 
-var View.visible
-    get() = visibility == VISIBLE
+var View?.visible
+    get() = this?.visibility == VISIBLE
     set(value) {
-        visibility = if (value) VISIBLE else GONE
+        this?.visibility = if (value) VISIBLE else GONE
     }
 
 /**
  * gone: Boolean = true
  */
-inline fun View.hide(gone: Boolean = true) {
-    visibility = if (gone) GONE else INVISIBLE
+inline fun View?.hide(gone: Boolean = true) {
+    this?.visibility = if (gone) GONE else INVISIBLE
 }
 
-inline fun View.show() {
-    visibility = VISIBLE
+inline fun View?.show() {
+    this?.visibility = VISIBLE
 }
 
-inline fun View.setWidth(width: Int) {
+inline fun View?.setWidth(width: Int) {
+    if (this == null) return
     val params = layoutParams
     params.width = width
     layoutParams = params
 }
 
-inline fun View.setHeight(height: Int) {
+inline fun View?.setHeight(height: Int) {
+    if (this == null) return
     val params = layoutParams
     params.height = height
     layoutParams = params
 }
 
-inline fun View.setSize(width: Int, height: Int) {
+inline fun View?.setSize(width: Int, height: Int) {
+    if (this == null) return
     val params = layoutParams
     params.width = width
     params.height = height
     layoutParams = params
 }
 
-fun View.setBackgroundTint(@ColorRes colorRes: Int) {
+fun View?.setBackgroundTint(@ColorRes colorRes: Int) {
+    if (this == null) return
     ViewCompat.setBackgroundTintList(
-            this, ColorStateList.valueOf(ContextCompat.getColor(this.context, colorRes))
+            this, ColorStateList.valueOf(ContextCompat.getColor(context, colorRes))
     )
 }
 
-fun List<View>.setOnClickListener(onClickListener: (l: View) -> Unit) {
-    forEach { it.setOnClickListener(onClickListener) }
+fun List<View?>.setOnClickListener(onClickListener: (l: View) -> Unit) {
+    forEach { it?.setOnClickListener(onClickListener) }
 }
 
-inline fun EditText.onTextChange(crossinline f: (s: CharSequence, start: Int, before: Int, count: Int) -> Unit) {
+inline fun EditText?.onTextChange(crossinline f: (s: CharSequence, start: Int, before: Int, count: Int) -> Unit) {
     val listener = object : KoiTextWatcher() {
         override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
             f(s, start, before, count)
         }
     }
-    this.addTextChangedListener(listener)
+    this?.addTextChangedListener(listener)
 }
 
 abstract class KoiTextWatcher : TextWatcher {
