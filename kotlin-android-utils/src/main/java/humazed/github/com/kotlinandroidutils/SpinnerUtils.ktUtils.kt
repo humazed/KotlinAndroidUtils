5b9543012b04db1f29535dfac152d08eb57877c2
ibrahim <humazed@gmail.com>
2020-10-18 16:42:53 +0200

    1.15.1 added SpinnerUtils.ktUtils

--- kotlin-android-utils/src/main/java/humazed/github/com/kotlinandroidutils/SpinnerUtils.ktUtils.kt
+++ kotlin-android-utils/src/main/java/humazed/github/com/kotlinandroidutils/SpinnerUtils.ktUtils.kt
@@ -3,11 +3,11 @@ package humazed.github.com.kotlinandroidutils
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
-import android.widget.AutoCompleteTextView
+import android.widget.Spinner
 
 
-fun <T> AutoCompleteTextView.onItemSelected(items: List<T>, names: List<String>, onItemSelected: (item: T, position: Int) -> Unit) {
-    setAdapter(ArrayAdapter(context, android.R.layout.simple_spinner_dropdown_item, names))
+fun <T> Spinner.onItemSelected(items: List<T>, names: List<String>, onItemSelected: (item: T, position: Int) -> Unit) {
+    adapter = ArrayAdapter(context, android.R.layout.simple_spinner_dropdown_item, names)
     onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
         override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
             onItemSelected(items[position], position)
@@ -18,15 +18,15 @@ fun <T> AutoCompleteTextView.onItemSelected(items: List<T>, names: List<String>,
     }
 }
 
-fun <T> AutoCompleteTextView.onItemSelected(items: List<T>, names: List<String>, onItemSelected: (item: T) -> Unit) {
+fun <T> Spinner.onItemSelected(items: List<T>, names: List<String>, onItemSelected: (item: T) -> Unit) {
     onItemSelected(items, names) { item, _ -> onItemSelected(item) }
 }
 
 
-fun <T> AutoCompleteTextView.onItemSelected(items: List<T>, mapNames: (T) -> String, onItemSelected: (item: T, position: Int) -> Unit) {
+fun <T> Spinner.onItemSelected(items: List<T>, mapNames: (T) -> String, onItemSelected: (item: T, position: Int) -> Unit) {
     onItemSelected(items, items.map(mapNames), onItemSelected)
 }
 
-fun <T> AutoCompleteTextView.onItemSelected(items: List<T>, mapNames: (T) -> String, onItemSelected: (item: T) -> Unit) {
+fun <T> Spinner.onItemSelected(items: List<T>, mapNames: (T) -> String, onItemSelected: (item: T) -> Unit) {
     onItemSelected(items, mapNames) { item, _ -> onItemSelected(item) }
 }
