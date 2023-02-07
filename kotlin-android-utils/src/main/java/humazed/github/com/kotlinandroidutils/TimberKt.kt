fa7bb830dc66a017919977bde317538180ef9097
Ibrahim Eid <humazed@gmail.com>
2023-02-07 13:40:15 +0200

    1.19.0
    update deps and targetSdkVersion 33

--- kotlin-android-utils/src/main/java/humazed/github/com/kotlinandroidutils/TimberKt.kt
+++ kotlin-android-utils/src/main/java/humazed/github/com/kotlinandroidutils/TimberKt.kt
@@ -178,7 +178,7 @@ inline fun wtf(t: Throwable) = Timber.wtf(t)
 /** @suppress */
 @PublishedApi
 internal inline fun log(block: () -> Unit) {
-    if (Timber.treeCount() > 0) block()
+    if (Timber.treeCount > 0) block()
 }
 
 
