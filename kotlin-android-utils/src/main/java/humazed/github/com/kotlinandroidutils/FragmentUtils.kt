4a86022f41abe493202ffccf97f8f2fe0afd2c1d
Ibrahim Eid <humazed@gmail.com>
2023-07-09 13:26:56 +0300

    1.20.5
    added withArguments to FragmentUtils.kt

--- kotlin-android-utils/src/main/java/humazed/github/com/kotlinandroidutils/FragmentUtils.kt
+++ kotlin-android-utils/src/main/java/humazed/github/com/kotlinandroidutils/FragmentUtils.kt
@@ -1,5 +1,8 @@
 package humazed.github.com.kotlinandroidutils
 
+import androidx.core.os.bundleOf
+import androidx.fragment.app.Fragment
+
 /**
  * User: YourPc
  * Date: 3/12/2018
@@ -9,4 +12,9 @@ inline fun androidx.fragment.app.FragmentManager.transaction(func: androidx.frag
     val transaction = beginTransaction()
     transaction.func()
     transaction.commit()
+}
+
+fun <T : Fragment> T.withArguments(vararg params: Pair<String, Any?>): T {
+    arguments = bundleOf(*params)
+    return this
 }
